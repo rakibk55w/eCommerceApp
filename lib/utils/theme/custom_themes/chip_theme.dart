880e6276c020baef2c915bdbff712b90ac0ebcc9
rakib <rakibk55w@gmail.com>
2025-05-02 01:07:44 +0600

    Created custom chip theme

---
+++ lib/utils/theme/custom_themes/chip_theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppChipTheme {
+  AppChipTheme._();
+
+  static ChipThemeData lightChipTheme = ChipThemeData(
+    disabledColor: Colors.grey.withAlpha((0.5 * 256).toInt()),
+    labelStyle: TextStyle(color: Colors.black),
+    selectedColor: Colors.blue,
+    padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+    checkmarkColor: Colors.white,
+  );
+
+  static ChipThemeData darkChipTheme = ChipThemeData(
+    disabledColor: Colors.grey,
+    labelStyle: TextStyle(color: Colors.white),
+    selectedColor: Colors.blue,
+    padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+    checkmarkColor: Colors.white,
+  );
+}
