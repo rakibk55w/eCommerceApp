a9cd19397bda7a2db96cb9294cd73b203fff17c8
rakib <rakibk55w@gmail.com>
2025-05-03 00:44:36 +0600

    Fixed some syntax mistakes

--- lib/utils/http/http_client.dart
+++ lib/utils/http/http_client.dart
@@ -1,8 +1,5 @@
 import 'dart:convert';
-
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:res';
 
 class AppHttpHelper {
   static const String _baseUrl = 'http://api-base-url.com';
