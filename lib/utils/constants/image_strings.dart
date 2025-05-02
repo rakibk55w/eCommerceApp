4cc4d9de089c773cfa742b10c9aaa991ea727209
rakib <rakibk55w@gmail.com>
2025-05-02 19:15:40 +0600

    Created path variables for logos

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,8 @@
+class AppImages{
+
+  static const String darkAppLogo = "assets/logos/t-store-splash-logo-black.png";
+  static const String lightAppLogo = "assets/logos/t-store-splash-logo-white.png";
+
+  static const String google = "assets/logos/google-icon.png";
+  static const String facebook = "assets/logos/facebook-icon.png";
+}
--- lib/utils/constants/sizes.dart
+++ lib/utils/constants/sizes.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-
 class AppSizes{
 
   static const double xs = 4.0;
