1b55dc9b8447067a303ea96366b8f23ef0ba1ee3
rakib <rakibk55w@gmail.com>
2025-05-02 18:25:53 +0600

    Created custom colors for the app

---
+++ lib/app.dart
@@ -0,0 +1,30 @@
+import 'package:e_commerce_app/utils/theme/theme.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.system,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello World"),
+        backgroundColor: Colors.blueAccent,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'app.dart';
+
 void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: ThemeData(),
-      darkTheme: ThemeData(),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Hello World"),
-        backgroundColor: Colors.blueAccent,
-      ),
-    );
-  }
-}
 
 
 
