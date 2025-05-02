170e763145efcb4e942061af6526d0c5386a494b
rakib <rakibk55w@gmail.com>
2025-05-02 19:24:03 +0600

    Created string variables

---
+++ lib/utils/constants/text_strings.dart
@@ -0,0 +1,13 @@
+class AppTexts{
+
+  static const String onBoardingTitle1 = "Choose your product";
+  static const String onBoardingTitle2 = "Select payment method";
+  static const String onBoardingTitle3 = "Deliver at your doorstep";
+
+  static const String onBoardingSubTitle1 = "Welcome to a world of lifetime choices - Your perfect product awaits";
+  static const String onBoardingSubTitle2 = "For seamless transactions, choose your payment path - Your convenience, your priority";
+  static const String onBoardingSubTitle3 = "From our doorstep to yours - Swift, secure and contactless delivery";
+
+  static const String homeAppbarTitle = "Good day for shopping";
+  static const String homeAppbarSubTitle = "User name";
+}
