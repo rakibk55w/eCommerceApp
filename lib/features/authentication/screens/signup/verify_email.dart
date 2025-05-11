d012bb02014ae068f36cb0bc28d69b838dabd1ea
rakib <rakibk55w@gmail.com>
2025-05-11 21:13:27 +0600

    Created verify email file. need to complete the UI

---
+++ lib/features/authentication/screens/signup/verify_email.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import '../../../../utils/constants/sizes.dart';
+
+class VerifyEmail extends StatelessWidget {
+  const VerifyEmail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(
+            AppSizes.defaultSpace
+          ),
+          child: Column(
+            children: [
+              //image
+
+              //title subtitle
+
+              //buttons
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
