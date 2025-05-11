9fbde93e4cd15e979611c3572ee5345a458538d6
rakib <rakibk55w@gmail.com>
2025-05-12 00:56:02 +0600

    Created forget password and reset password screen

--- lib/features/authentication/screens/login/widgets/login_form.dart
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_app/features/authentication/screens/password_configuration/forget_password.dart';
 import 'package:e_commerce_app/features/authentication/screens/signup/signup.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -53,7 +54,9 @@ class AppLoginForm extends StatelessWidget {
 
                 //Forget password
                 TextButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Get.to(const ForgetPassword());
+                  },
                   child: const Text(AppTexts.forgetPassword),
                 ),
               ],
