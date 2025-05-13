87293d5fa6a914e79088ff8f1f2f9ee5b3b20307
rakib <rakibk55w@gmail.com>
2025-05-14 00:42:27 +0600

    Added custom curved shape for header section of home page

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,10 +1,26 @@
+import 'package:e_commerce_app/common/widgets/curved_edges.dart';
+import 'package:e_commerce_app/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../common/widgets/circular_container.dart';
+import '../../../../common/widgets/curved_edges_widget.dart';
+import '../../../../common/widgets/primary_header_container.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            AppPrimaryHeaderContainer(
+              child: Container()
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
