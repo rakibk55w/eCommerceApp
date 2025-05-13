22ec8cb43e88da479ceb3df302e24bc2da238f8d
rakib <rakibk55w@gmail.com>
2025-05-14 00:44:10 +0600

    Removed unnecessary imports for optimization

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,9 +1,5 @@
-import 'package:e_commerce_app/common/widgets/curved_edges.dart';
-import 'package:e_commerce_app/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../common/widgets/circular_container.dart';
-import '../../../../common/widgets/curved_edges_widget.dart';
 import '../../../../common/widgets/primary_header_container.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -14,11 +10,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
-          children: [
-            AppPrimaryHeaderContainer(
-              child: Container()
-            ),
-          ],
+          children: [AppPrimaryHeaderContainer(child: Container())],
         ),
       ),
     );
