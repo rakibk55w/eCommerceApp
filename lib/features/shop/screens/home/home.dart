8d69b160ccb73ef0c5277af22d65795ce245d263
rakib <rakibk55w@gmail.com>
2025-05-14 20:47:11 +0600

    Created a searchbar

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,5 +1,10 @@
 import 'package:e_commerce_app/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:e_commerce_app/utils/constants/colors.dart';
+import 'package:e_commerce_app/utils/constants/sizes.dart';
+import 'package:e_commerce_app/utils/device/device_utility.dart';
+import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 import '../../../../common/widgets/primary_header_container.dart';
 
@@ -19,12 +24,64 @@ class HomeScreen extends StatelessWidget {
                   //Appbar
                   AppHomeAppbar(),
 
+                  const SizedBox(height: AppSizes.spaceBetweenSections),
+
                   //Searchbar
+                  AppSearchContainer(
+                    text: 'Search in store',
+                    icon: Iconsax.search_normal,
+                    showBackground: true,
+                    showBorder: true,
+                  ),
 
                   //Categories
                 ],
               ),
             ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AppSearchContainer extends StatelessWidget {
+  const AppSearchContainer({
+    super.key,
+    required this.text,
+    this.icon,
+    required this.showBackground,
+    required this.showBorder,
+  });
+
+  final String text;
+  final IconData? icon;
+  final bool showBackground, showBorder;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = AppHelperFunctions.isDarkMode(context);
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: AppSizes.defaultSpace),
+      child: Container(
+        width: AppDeviceUtils.getScreenWidth(context),
+        padding: const EdgeInsets.all(AppSizes.md),
+        decoration: BoxDecoration(
+          color:
+              showBackground
+                  ? (dark ? AppColors.dark : AppColors.light)
+                  : Colors.transparent,
+          borderRadius: BorderRadius.circular(AppSizes.cardRadiusLg),
+          border: showBorder ? Border.all(color: AppColors.grey) : null,
+        ),
+        child: Row(
+          children: [
+            Icon(icon, color: AppColors.darkerGrey),
+
+            const SizedBox(width: AppSizes.spaceBetweenItems),
+
+            Text(text, style: Theme.of(context).textTheme.bodySmall),
           ],
         ),
       ),
