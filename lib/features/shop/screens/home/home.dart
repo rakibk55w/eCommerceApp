90a623fe614988624244db7f033e528866bd3808
rakib <rakibk55w@gmail.com>
2025-05-15 22:32:03 +0600

    Created controller for updating dot indicators of the carousel when images are slid. Also did some refactoring

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,16 +1,14 @@
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:e_commerce_app/common/widgets/circular_container.dart';
+import 'package:e_commerce_app/features/shop/screens/home/widgets/carousel_slider.dart';
 import 'package:e_commerce_app/features/shop/screens/home/widgets/home_appbar.dart';
 import 'package:e_commerce_app/features/shop/screens/home/widgets/home_category.dart';
 import 'package:e_commerce_app/utils/constants/colors.dart';
-import 'package:e_commerce_app/utils/constants/image_strings.dart';
 import 'package:e_commerce_app/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../common/widgets/primary_header_container.dart';
-import '../../../../common/widgets/rounded_image.dart';
 import '../../../../common/widgets/search_container.dart';
 import '../../../../common/widgets/section_heading.dart';
+import '../../../../utils/constants/image_strings.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -60,34 +58,16 @@ class HomeScreen extends StatelessWidget {
             //Body
             Padding(
               padding: const EdgeInsets.all(AppSizes.defaultSpace),
-              child: Column(
-                children: [
-                  CarouselSlider(
-                    options: CarouselOptions(viewportFraction: 1),
-                    items: [
-                      AppRoundedImage(imageUrl: AppImages.promoBanner1),
-                      AppRoundedImage(imageUrl: AppImages.promoBanner2),
-                      AppRoundedImage(imageUrl: AppImages.promoBanner3),
-                      AppRoundedImage(imageUrl: AppImages.promoBanner4),
-                      AppRoundedImage(imageUrl: AppImages.promoBanner5),
-                      AppRoundedImage(imageUrl: AppImages.promoBanner6),
-                      AppRoundedImage(imageUrl: AppImages.promoBanner7),
-                      AppRoundedImage(imageUrl: AppImages.promoBanner8),
-                    ],
-                  ),
-                  const SizedBox(height: AppSizes.spaceBetweenItems),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      for (int i = 0; i < 8; i++)
-                        const AppCircularContainer(
-                          width: 10,
-                          height: 10,
-                          margin: EdgeInsets.only(right: 10),
-                          backgroundColor: Colors.green,
-                        ),
-                    ],
-                  ),
+              child: AppCarouselSlider(
+                banners: [
+                  AppImages.promoBanner1,
+                  AppImages.promoBanner2,
+                  AppImages.promoBanner3,
+                  AppImages.promoBanner4,
+                  AppImages.promoBanner5,
+                  AppImages.promoBanner6,
+                  AppImages.promoBanner7,
+                  AppImages.promoBanner8,
                 ],
               ),
             ),
---
+++ lib/features/shop/screens/home/widgets/carousel_slider.dart
@@ -0,0 +1,53 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:e_commerce_app/features/shop/controllers/home_controller.dart';
+import 'package:e_commerce_app/utils/constants/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../../../common/widgets/circular_container.dart';
+import '../../../../../common/widgets/rounded_image.dart';
+import '../../../../../utils/constants/image_strings.dart';
+import '../../../../../utils/constants/sizes.dart';
+
+class AppCarouselSlider extends StatelessWidget {
+  const AppCarouselSlider({super.key, required this.banners});
+
+  final List<String> banners;
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(HomeController());
+
+    return Column(
+      children: [
+        CarouselSlider(
+          options: CarouselOptions(
+            viewportFraction: 1,
+            onPageChanged: (index, _) {
+              controller.updatePageIndicator(index);
+            },
+          ),
+          items: banners.map((url) => AppRoundedImage(imageUrl: url)).toList(),
+        ),
+        const SizedBox(height: AppSizes.spaceBetweenItems),
+        Obx(() {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              for (int i = 0; i < banners.length; i++)
+                AppCircularContainer(
+                  width: 10,
+                  height: 10,
+                  margin: const EdgeInsets.only(right: 10),
+                  backgroundColor:
+                      controller.carouselCurrentIndex.value == i
+                          ? AppColors.primary
+                          : AppColors.grey,
+                ),
+            ],
+          );
+        }),
+      ],
+    );
+  }
+}
