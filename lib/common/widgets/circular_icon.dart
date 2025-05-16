19d6f2726bbfa1daebc2908537a26db99159d91b
rakib <rakibk55w@gmail.com>
2025-05-16 22:59:04 +0600

    Created custom rounded icon and product text template

---
+++ lib/common/widgets/circular_icon.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../../utils/constants/colors.dart';
+import '../../utils/constants/sizes.dart';
+import '../../utils/helpers/helper_functions.dart';
+
+class AppCircularIcon extends StatelessWidget {
+  const AppCircularIcon({
+    super.key,
+    this.width,
+    this.height,
+    this.size = AppSizes.lg,
+    required this.icon,
+    this.color,
+    this.backgroundColor,
+    this.onPressed,
+  });
+
+  final double? width, height, size;
+  final IconData icon;
+  final Color? color;
+  final Color? backgroundColor;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = AppHelperFunctions.isDarkMode(context);
+
+    return Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+        color:
+            backgroundColor ??
+            (dark
+                ? AppColors.black.withAlpha((255 * 0.9).toInt())
+                : AppColors.white.withAlpha((255 * 0.9).toInt())),
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: IconButton(
+        onPressed: onPressed,
+        icon: Icon(icon, size: size, color: color),
+      ),
+    );
+  }
+}
