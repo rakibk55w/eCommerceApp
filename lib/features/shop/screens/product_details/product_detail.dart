1441c648cf4e8486cb69f83bb02849dfbdae80ed
rakib <rakibk55w@gmail.com>
2025-05-20 00:18:26 +0600

    Created rating and share widget

--- lib/features/shop/screens/product_details/product_detail.dart
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -1,7 +1,10 @@
 import 'package:e_commerce_app/features/shop/screens/product_details/widgets/product_detail_image_slider.dart';
+import 'package:e_commerce_app/features/shop/screens/product_details/widgets/rating_share_widget.dart';
 import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
+import '../../../../utils/constants/sizes.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   const ProductDetailScreen({super.key});
@@ -10,15 +13,38 @@ class ProductDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final dark = AppHelperFunctions.isDarkMode(context);
 
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             //Product image slider
             AppProductImageSlider(),
-          ],
 
-          //Product details
+            //Product details
+            Padding(
+              padding: EdgeInsets.only(
+                right: AppSizes.defaultSpace,
+                left: AppSizes.defaultSpace,
+                bottom: AppSizes.defaultSpace,
+              ),
+              child: Column(
+                children: [
+                  //Rating and Share
+                  AppRatingAndShare(),
+
+                  //Price, Title. Stock, Brand
+
+                  //Attributes
+
+                  //Checkout button
+
+                  //Description
+
+                  //Reviews
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
