021cc3616fc481004ae66749369cad610be586cd
rakib <rakibk55w@gmail.com>
2025-05-23 23:07:33 +0600

    Added order progress, order date, order number and delivery date. Also an icon to see order details

--- lib/features/shop/screens/order/widgets/order_list.dart
+++ lib/features/shop/screens/order/widgets/order_list.dart
@@ -12,43 +12,122 @@ class AppOrderListItems extends StatelessWidget {
   Widget build(BuildContext context) {
     final dark = AppHelperFunctions.isDarkMode(context);
 
-    return AppRoundedContainer(
-      showBorder: true,
-      backgroundColor: dark ? AppColors.dark : AppColors.light,
-      padding: EdgeInsets.all(AppSizes.md),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Row(
-            children: [
-              //Icon
-              Icon(Iconsax.ship),
-              SizedBox(width: AppSizes.spaceBetweenItems / 2),
+    return ListView.separated(
+      itemCount: 5,
+      shrinkWrap: true,
+      separatorBuilder: (_, __) => SizedBox(height: AppSizes.spaceBetweenItems),
+      itemBuilder:
+          (_, index) => AppRoundedContainer(
+            showBorder: true,
+            backgroundColor: dark ? AppColors.dark : AppColors.light,
+            padding: EdgeInsets.all(AppSizes.md),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                //Row 1
+                Row(
+                  children: [
+                    //Icon
+                    Icon(Iconsax.ship),
+                    SizedBox(width: AppSizes.spaceBetweenItems / 2),
 
-              //Status and Date
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    'Processing',
-                    style: Theme.of(context).textTheme.bodyLarge!.apply(
-                      color: AppColors.primary,
-                      fontWeightDelta: 1,
+                    //Status and Date
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            'Processing',
+                            style: Theme.of(context).textTheme.bodyLarge!.apply(
+                              color: AppColors.primary,
+                              fontWeightDelta: 1,
+                            ),
+                          ),
+                          Text(
+                            '07 Nov, 2024',
+                            style: Theme.of(context).textTheme.headlineSmall,
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  Text(
-                    '07 Nov, 2024',
-                    style: Theme.of(context).textTheme.headlineSmall,
-                  ),
-                ],
-              ),
-            ],
-          ),
 
-          Row(),
-        ],
-      ),
+                    //Details icon
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Iconsax.arrow_right_34, size: AppSizes.iconSm),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: AppSizes.spaceBetweenItems),
+
+                //Row 2
+                Row(
+                  children: [
+                    Expanded(
+                      child: Row(
+                        children: [
+                          //Icon
+                          Icon(Iconsax.tag),
+                          SizedBox(width: AppSizes.spaceBetweenItems / 2),
+
+                          //Status and Date
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  'Order',
+                                  style:
+                                      Theme.of(context).textTheme.labelMedium,
+                                ),
+                                Text(
+                                  '[#256f2]',
+                                  style:
+                                      Theme.of(context).textTheme.titleMedium,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Row(
+                        children: [
+                          //Icon
+                          Icon(Iconsax.calendar),
+                          SizedBox(width: AppSizes.spaceBetweenItems / 2),
+
+                          //Status and Date
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  'Shipping Date',
+                                  style:
+                                      Theme.of(context).textTheme.labelMedium,
+                                ),
+                                Text(
+                                  '02 Feb, 2025',
+                                  style:
+                                      Theme.of(context).textTheme.titleMedium,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
     );
   }
 }
