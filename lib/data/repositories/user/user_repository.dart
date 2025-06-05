be2fee8bb6fd85c085e9188170e0f73258d391e6
rakib <rakibk55w@gmail.com>
2025-06-05 21:15:17 +0600

    Added functionality to fetch, update and delete user data from firestore

--- lib/data/repositories/user/user_repository.dart
+++ lib/data/repositories/user/user_repository.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:e_commerce_app/data/repositories/authentication/authentication_repository.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:e_commerce_app/features/authentication/models/user_model.dart';
@@ -12,9 +13,76 @@ class UserRepository extends GetxController {
 
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
+  //Save user data to firestore
   Future<void> saveUserRecord(UserModel user) async {
     try {
       await _db.collection("Users").doc(user.id).set(user.toJson());
+    } on FirebaseException catch (e) {
+      throw AppFirebaseExceptions(e.code).message;
+    } on FormatException catch (_) {
+      throw const AppFormatException();
+    } on PlatformException catch (e) {
+      throw AppPlatformException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again later.';
+    }
+  }
+
+  //Fetch user data from firestore
+  Future<UserModel> fetchUserDetails() async {
+    try {
+      final documentSnapshot = await _db.collection("Users").doc(AuthenticationRepository.instance.authUser?.uid).get();
+      if (documentSnapshot.exists) {
+        return UserModel.fromSnapshot(documentSnapshot);
+      }
+      else{
+        return UserModel.empty();
+      }
+    } on FirebaseException catch (e) {
+      throw AppFirebaseExceptions(e.code).message;
+    } on FormatException catch (_) {
+      throw const AppFormatException();
+    } on PlatformException catch (e) {
+      throw AppPlatformException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again later.';
+    }
+  }
+
+  //Update user data in firestore
+  Future<void> updateUserDetails(UserModel updatedUser) async {
+    try {
+      await _db.collection("Users").doc(updatedUser.id).update(updatedUser.toJson());
+    } on FirebaseException catch (e) {
+      throw AppFirebaseExceptions(e.code).message;
+    } on FormatException catch (_) {
+      throw const AppFormatException();
+    } on PlatformException catch (e) {
+      throw AppPlatformException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again later.';
+    }
+  }
+
+  //Update a single field in firestore
+  Future<void> updateSingleField(Map<String, dynamic> json) async {
+    try {
+      await _db.collection("Users").doc(AuthenticationRepository.instance.authUser?.uid).update(json);
+    } on FirebaseException catch (e) {
+      throw AppFirebaseExceptions(e.code).message;
+    } on FormatException catch (_) {
+      throw const AppFormatException();
+    } on PlatformException catch (e) {
+      throw AppPlatformException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again later.';
+    }
+  }
+
+  //Delete user data in firestore
+  Future<void> removeUserDetails(String userId) async {
+    try {
+      await _db.collection("Users").doc(userId).delete();
     } on FirebaseException catch (e) {
       throw AppFirebaseExceptions(e.code).message;
     } on FormatException catch (_) {
